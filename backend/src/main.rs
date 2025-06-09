510f3c138f9b7bf03f216cbd2a4a0e5025d10e10
Tom Geelen <t.f.g.geelen@gmail.com>
2025-06-09 21:53:21 +0000

    Get a webserver running

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,4 @@
+
 use axum::{
     routing::get,
     Router,
@@ -6,9 +7,20 @@ use axum::{
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(homepage))
+        .route("/about", get(about_page));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:5900").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+// Homepage handler
+async fn homepage() -> &'static str {
+    "Welcome to My Rust Website!"
+}
+// About page handler
+async fn about_page() -> &'static str {
+    "This is the About Page of the Rust Website."
 }
